b412d113ac90430c55f686644215d6ab0b7de891
aegarmus <aegarmus@gmail.com>
2024-12-06 21:54:01 -0300

    refact: Crud abstract functions to create records

--- src/models/Usuario.model.js
+++ src/models/Usuario.model.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid'
 import { query } from '../config/db.config.js';
 import { DataBaseError, ValidationError } from '../errors/TypesError.js';
 import { Validation } from '../utils/validate/Validate.js';
+import { createRecord } from '../utils/crud/crudUtils.js';
 
 
 export class Usuario {
@@ -70,22 +71,15 @@ export class Usuario {
 
     static async create(data) {
         try {
-
             /*  Usuario.validate(data); */
-
-            const { nombre, apellido_paterno, apellido_materno, email, telefono } = data;
             const id = uuidv4();
             const active = true;
 
-            const insertQuery = `
-                INSERT INTO usuarios (id, nombre, apellido_paterno, apellido_materno, email, telefono, active)
-                VALUES ($1, $2, $3, $4, $5, $6, $7)
-                RETURNING *;
-            `
-            const values = [id, nombre, apellido_paterno, apellido_materno, email, telefono, active];
+            const user = { id, ...data, active }
 
-            const { rows } = await query(insertQuery, values);
-            return rows[0]
+            const userRecorded = await createRecord('usuarios', user)
+            return userRecorded
+
         } catch (error) {
             throw new DataBaseError('Error al registrar el usuario en la base de datos', error)
         }
