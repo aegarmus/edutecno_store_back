6c5cbf5c597af279b1284ea3f5653deeb2cb6a44
aegarmus <aegarmus@gmail.com>
2024-12-09 20:21:04 -0300

    feat: Get active records created and implemented in User

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createUser } from '../controllers/usuario.controller.js';
+import { createUser, findAllActiveUsers, findUserActiveById } from '../controllers/usuario.controller.js';
 import { validationMiddleware } from '../middlewares/validate.middleware.js';
 import { Usuario } from '../models/Usuario.model.js';
 
@@ -7,6 +7,8 @@ import { Usuario } from '../models/Usuario.model.js';
 const router = Router();
 
 router.post('/usuario', validationMiddleware(Usuario.validate), createUser);
+router.get('/usuario', findAllActiveUsers);
+router.get('/usuario/:id', findUserActiveById)
 
 
 export default router
--- src/utils/constants/createTablesQueries.js
+++ src/utils/constants/createTablesQueries.js
@@ -64,4 +64,4 @@ export const createTableQueries = [
         `,
     name: "ventas_productos",
   },
-];
+]; 
